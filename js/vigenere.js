7341d8cdaa68b75dbd34ab14285f081b1bb08355
EmilianoDev12 <gcasillasa1900@alumno.ipn.mx>
2021-09-28 00:08:02 -0500

    juan

--- js/vigenere.js
+++ js/vigenere.js
@@ -37,3 +37,13 @@ function procesarV(operacion){
 
     return false;
 }
+
+function validarc(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /[a-z]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
